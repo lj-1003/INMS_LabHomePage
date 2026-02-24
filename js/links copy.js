9b11e4ec3ff2caaa6ea3cc1599c74e207ef4396c
lj-1003 <levijustin24@gmail.com>
2026-02-24 14:29:17 -0600

    New JSON formatting.

---
+++ js/links copy.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", async () => {
+  const links = await getLinks();
+  renderLinks(links);
+});
+
+//Get from json
+async function getLinks() {
+    const response = await fetch("../data/links.json");
+    const data = await response.json();
+    return data;
+}
+
+//Render
+function renderLinks(links) {
+
+    //Test to see if getting data
+    console.log("Loading links...")
+    links.forEach((link) => {
+    console.log(`   ${link.title}: ${link.url}`);
+    })
+
+    //For real
+    const container = document.getElementById("link-btn-grid");
+    container.innerHTML = "";
+    links.forEach((link) => {
+        const div = document.createElement("div");
+        div.className = "waves-effect waves-light btn fhsu-gold black-text";
+        div.innerHTML = `
+        <a href="${link.url}" 
+            target="_blank">${link.url}
+        </a>
+        `;
+        container.appendChild(div);
+    });
+}
--- js/links.js
+++ js/links.js
@@ -5,32 +5,34 @@ document.addEventListener("DOMContentLoaded", async () => {
 
 //Get from json
 async function getLinks() {
-    const response = await fetch("../data/links.json");
+    const response = await fetch("../data/data.json");
     const data = await response.json();
     return data;
 }
 
 //Render
 function renderLinks(links) {
+    student = links.student; //student resources array
+    lab = links.lab; //lab resources array
+    
+    //Test to see if getting data---------------------------
+    console.log("Loading links...");
+    console.log(links);
+    console.log(student);
+    console.log(lab);
+    //-------------------------------------------------------
 
-    //Test to see if getting data
-    console.log("Loading links...")
-    links.forEach((link) => {
-    console.log(`   ${link.title}: ${link.url}`);
-    })
-
-    //For real
-    const container = document.getElementById("link-btn-grid");
+    //Render on Page
+    const container = document.getElementById("link-btn-grid1");
     container.innerHTML = "";
-    links.forEach((link) => {
+    student.forEach((student) => {
         const div = document.createElement("div");
         div.className = "waves-effect waves-light btn fhsu-gold black-text";
         div.innerHTML = `
-        <a href="${link.url}" 
-            target="_blank">${link.url}
-            class="waves-effect waves-light btn fhsu-gold black-text">
+        <a href="${student.url}" 
+            target="_blank">${student.title}
         </a>
         `;
         container.appendChild(div);
     });
-}
+    }
